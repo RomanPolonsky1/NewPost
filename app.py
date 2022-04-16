04edb973aee3812850d0f34942aab0b617cac568
polonskys <romanpolonsky@me.com>
2022-04-16 18:07:04 +0300

    HTML template for writing a post

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<a href="/new-item">Write Post</a>'
+
+
+@app.route('/new-item')
+def new_item():
+    return render_template('write-post.html')
 
 
 if __name__ == '__main__':
